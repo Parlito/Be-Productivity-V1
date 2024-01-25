0daf108e50a80d4a302cfdd8e4235972822615f3
Paulo Henrique <contatopauloeditor@gmail.com>
2024-01-25 18:40:32 -0300

    Criação da flatlist do Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Container_2, SimpleButton, SimpleText1, SimpleText2, Title } from './styled'
 import { useNavigation } from '@react-navigation/native'
+
+import { TaskModel } from '../../databases/model/taskModel';
+import { database } from '../../databases';
+import { FlatList } from 'react-native';
+import { Task } from '../Task';
 
 function Card() {
 
   const navigation = useNavigation();
 
+  const [tasks, setTasks] = useState<TaskModel[]>();
+
+  async function fetchData() {
+      const taskCollection = database.get<TaskModel>('task');
+      const response = await taskCollection.query().fetch();
+      setTasks(response);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
   return (
     <Container>
       <Title>Tarefas do dia</Title>
+      {tasks && tasks.length > 0 ? 
+        (
+          <FlatList
+            data={tasks}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Task data={item} onEdit={() => {}} onRemove={() => {}} />
+            )}
+          />
+        ) 
+        : 
+        (
         <Container_2>
-        
-        <SimpleText1>Aparentemente você não possui nenhuma tarefa adicionada 😓</SimpleText1>
+          <SimpleText1>Aparentemente você não possui nenhuma tarefa adicionada 😓</SimpleText1>
         </Container_2>   
+        )}
 
         <SimpleButton onPress={() => navigation.navigate('formu')}>
             <SimpleText2>Criar tarefa😁</SimpleText2>
