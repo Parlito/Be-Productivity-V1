a1f8a082e224059446b702afc6b798003ff3eb6f
Paulo Henrique <contatopauloeditor@gmail.com>
2024-01-25 12:31:40 -0300

    Criação da página de formulário

---
+++ src/screens/Formu/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Text_1 } from "./styled"
+import { StatusBar } from 'react-native';
+
+export default function Formu(){
+    return (
+        <Container>
+            <StatusBar backgroundColor="#0F0F0F"/>
+            <Text_1>Crie sua tarefa</Text_1>
+        </Container>
+    )
+}
