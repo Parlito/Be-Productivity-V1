ef51c51d24ca1fb684b440612ce2ff12400adb21
Paulo Henrique <contatopauloeditor@gmail.com>
2024-01-25 12:06:18 -0300

    Criação da página Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+
+import { Container, Text_1 } from "./styled";
+
+export default function Home(){
+    return (
+        <Container>
+            <Text_1>Be Productivity</Text_1>
+
+        </Container>
+    )
+}
---
+++ src/screens/Home/styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #0F0F0F;
+    padding: 20px;
+`;
+
+export const Text_1 = styled.Text`
+    font-size: 50px;
+    font-weight: bold;
+    color: #008170;
+    text-align: center;
+`;
