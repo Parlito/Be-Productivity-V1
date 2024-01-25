e32c0d31917501386a1545e866c27b5c102a6eca
Paulo Henrique <contatopauloeditor@gmail.com>
2024-01-25 13:34:46 -0300

    Adicionando a função do botão

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Container, Container_2, SimpleButton, SimpleText1, SimpleText2, Title } from './styled'
+import { useNavigation } from '@react-navigation/native'
 
 function Card() {
+
+  const navigation = useNavigation();
+
   return (
     <Container>
       <Title>Tarefas do dia</Title>
@@ -10,7 +14,7 @@ function Card() {
         <SimpleText1>Aparentemente você não possui nenhuma tarefa adicionada 😓</SimpleText1>
         </Container_2>   
 
-        <SimpleButton>
+        <SimpleButton onPress={() => navigation.navigate('formu')}>
             <SimpleText2>Criar tarefa😁</SimpleText2>
         </SimpleButton>
         
