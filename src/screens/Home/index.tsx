3338f182987051cb7b8b674a7d2b9cc56a4f08a2
Paulo Henrique <contatopauloeditor@gmail.com>
2024-01-25 12:13:44 -0300

    página definida

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
 
 import { Container, Text_1 } from "./styled";
+import { StatusBar } from 'react-native';
 
 export default function Home(){
     return (
+        <>
+        <StatusBar backgroundColor="#0F0F0F"/>
         <Container>
             <Text_1>Be Productivity</Text_1>
-
         </Container>
+        </>
     )
 }
