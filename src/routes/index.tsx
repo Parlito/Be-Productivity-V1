905c75fc52037147eae12351bd6cdcee5e40faf1
Paulo Henrique <contatopauloeditor@gmail.com>
2024-01-25 13:09:59 -0300

    Modificando as rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Formu from '../screens/Formu';
 
-const Stack = createNativeStackNavigator();
-
 function Routes() {
+
+  const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator 
+      initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
         <Stack.Screen name="Formu" component={Formu}/>
       </Stack.Navigator>
     </NavigationContainer>
